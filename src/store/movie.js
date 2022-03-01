7b693c75210375fee6e193c3efb46b4b1e10d5fb
ferain <ferain@kakao.com>
2022-03-01 15:58:13 +0900

    표출되는 데이터 수 컨트롤

--- src/store/movie.js
+++ src/store/movie.js
@@ -24,14 +24,29 @@ export default {
   },
   //state(data) 변이 외 (비동기)
   actions: {
-    async searchMovies({commit}, payload) {  //context => {commit}
+    async searchMovies({state, commit}, payload) {  //context => {commit} //state도 동일
       const {title, type, number, year} = payload
       const OMDB_API_KEY = '7035c60c'
       const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`) 
-      const {Search, totalResults } = res.data
       commit('updateState', { //context.commit => commit
         movies: Search
       })
+
+      const {Search, totalResults } = res.data
+      const total = parseInt(totalResults, 10) //int로 데이터 변환 (10진수)
+      const pageLength = Math.ceil(total/10)
+      if (pageLength > 1) {
+        for (let page = 2; page <= pageLength; page =+ 1){
+          if (page>number / 10){
+            break
+          }
+          const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
+          const {Search} = res.data
+          commit('updateState', {
+            movies: [...state.moives, ...Search]  //... 전개 연산자
+          }) 
+        }
+      }
     }
   }
 }
